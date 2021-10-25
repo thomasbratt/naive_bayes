c27a82690ab9d68f6a3667f39262d7d5b6692a7c
Thomas Bratt <thomas.bratt@gmail.com>
2021-10-25 13:21:49 +0100

    Replace integer counts with f64, to avoid later casting

--- src/learner.rs
+++ src/learner.rs
@@ -9,19 +9,19 @@ where
     D: Copy + Eq + Hash,
     H: Copy + Eq + Hash,
 {
-    count_hypothesis: HashMap<H, u64>,
-    count_likelihood: HashMap<(D, H), u64>,
-    count_total: u64,
+    count_hypothesis: HashMap<H, f64>,
+    count_likelihood: HashMap<(D, H), f64>,
+    count_total: f64,
 }
 
 impl<D: Copy + Eq + Hash, H: Copy + Eq + Hash> Learner<D, H> {
     pub fn update_online(&mut self, datum: D, hypothesis: H) -> &mut Self {
-        *self.count_hypothesis.entry(hypothesis).or_insert(0) += 1;
+        *self.count_hypothesis.entry(hypothesis).or_insert(0.0) += 1.0;
         *self
             .count_likelihood
             .entry((datum, hypothesis))
-            .or_insert(0) += 1;
-        self.count_total += 1;
+            .or_insert(0.0) += 1.0;
+        self.count_total += 1.0;
         self
     }
 
@@ -36,22 +36,27 @@ impl<D: Copy + Eq + Hash, H: Copy + Eq + Hash> Learner<D, H> {
         let priors: HashMap<H, f64> = self
             .count_hypothesis
             .iter()
-            .map(|(h, c)| (*h, *c as f64 / self.count_total as f64))
+            .map(|(h, c)| (*h, (*c / self.count_total).log2()))
             .collect();
 
+        // TODO: product_of_conditionals
         let mut probabilities_as_map: HashMap<D, HashMap<H, f64>> = HashMap::default();
         for ((datum, hypothesis), count) in &self.count_likelihood {
+            // TODO: remove prior_h
             let prior_h = *priors.get(hypothesis).unwrap();
-            let p: f64 = prior_h * (*count as f64 / self.count_total as f64);
+            let p: f64 = prior_h + (*count / self.count_total).log2();
             probabilities_as_map
                 .entry(*datum)
                 .or_insert_with(HashMap::default)
                 .insert(*hypothesis, p);
         }
 
+        // TODO: product_of_conditionals
+        // TODO: add prior_h back here
         let probabilities: HashMap<D, Vec<(H, f64)>> = probabilities_as_map
             .iter()
             .map(|(datum, hp)| {
+
                 let v = hp.iter().map(|x| (*x.0, *x.1)).collect();
                 (*datum, v)
             })
